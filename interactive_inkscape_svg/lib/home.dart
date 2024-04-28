051b145ad2a7a6bc8b3c8386e8bf3f453710dcb0
tkadams1 <46609177+tkadams1@users.noreply.github.com>
2024-04-28 16:02:39 -0500

    code cleanup

--- interactive_inkscape_svg/lib/home.dart
+++ interactive_inkscape_svg/lib/home.dart
@@ -30,33 +30,38 @@ class _HomeState extends State<Home> {
       });
     }
 
-    return Container(
-      alignment: Alignment.center,
-      // Widget content goes here
-      height: 500,
-      width: 350,
-      child: InteractiveViewer(
-        maxScale: 1,
-        minScale: 1,
-        child: Stack(
-          children: [
-            for (var country in widget.countries)
-              _getClippedImage(
-                clipper: Clipper(
-                  svgPath: country.path,
-                ),
-                color: Color(int.parse('FF${country.color}', radix: 16))
-                    .withOpacity(currentCountry == null
-                        ? 0.3
-                        : currentCountry.id == country.id
-                            ? 1.0
-                            : 0.3),
-                country: country,
-                onCountrySelected: onCountrySelected,
-              ),
-          ],
+    return Column(
+      children: [
+        Container(
+          alignment: Alignment.center,
+          // Widget content goes here
+          height: 500,
+          width: 350,
+          child: InteractiveViewer(
+            maxScale: 1,
+            minScale: 1,
+            child: Stack(
+              children: [
+                for (var country in widget.countries)
+                  _getClippedImage(
+                    clipper: Clipper(
+                      svgPath: country.path,
+                    ),
+                    color: Color(int.parse('FF${country.color}', radix: 16))
+                        .withOpacity(currentCountry == null
+                            ? 0.3
+                            : currentCountry.id == country.id
+                                ? 1.0
+                                : 0.3),
+                    country: country,
+                    onCountrySelected: onCountrySelected,
+                  ),
+              ],
+            ),
+          ),
         ),
-      ),
+        
+      ],
     );
   }
 
@@ -82,78 +87,7 @@ class _HomeState extends State<Home> {
       ),
   );
 }
-//Made outline but broke gestureDetector
-//   Widget _getClippedImage({
-//   required Clipper clipper,
-//   required Color color,
-//   required Country country,
-//   required Function(Country) onCountrySelected,
-// }) {
-//   return GestureDetector(
-//     onTap: () => onCountrySelected(country),
-//     child: CustomPaint(
-//       painter: _OutlinePainter(clipper: clipper, color: color),
-//       child: ClipPath(
-//         clipper: clipper,
-//         child: Container(
-//           color: color,
-//         ),
-//       ),
-//     ),
-//   );
-// }
-
   
-}
-class _StrokePainter extends CustomPainter {
-  final Color color;
-  final double strokeWidth;
-
-  _StrokePainter({required this.color, required this.strokeWidth});
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()
-      ..color = color
-      ..strokeWidth = strokeWidth
-      ..style = PaintingStyle.stroke;
-
-    final path = Path()
-      ..addRect(Rect.fromLTWH(0, 0, size.width, size.height));
-
-    canvas.drawPath(path, paint);
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
-}
-
-class ShapePainter extends CustomPainter {
-  final Clipper clipper;
-  final Color color;
-
-  ShapePainter({required this.clipper, required this.color});
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    var paint = Paint()
-      ..color = color
-      ..style = PaintingStyle.fill;
-
-    var outlinePaint = Paint()
-      ..color = Colors.black
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 2.0;
-
-    var path = clipper.getClip(size);
-    canvas.drawPath(path, paint);
-    canvas.drawPath(path, outlinePaint);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return false;
-  }
 }
 
 class _OutlinePainter extends CustomPainter {
